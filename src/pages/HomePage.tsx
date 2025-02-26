a7d6425dbafcee7a8609ce5c4aaccec04faa2ede
JRebel <jbaptiste.revel@gmail.com>
2025-02-26 20:43:29 +0100

    video back ground + 5 icon

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -1,6 +1,11 @@
 import React, { useCallback, useRef } from 'react';
 import { IonIcon } from '@ionic/react';
 import SchoolIcon from "@mui/icons-material/School"
+import EngineeringIcon from '@mui/icons-material/Engineering';
+import TerminalIcon from '@mui/icons-material/Terminal';
+import ContactMailIcon from '@mui/icons-material/ContactMail';
+import SportsGymnasticsIcon from '@mui/icons-material/SportsGymnastics';
+import BookmarksIcon from '@mui/icons-material/Bookmarks';
 import './../css/homePageStyle.css'
 
 
@@ -38,18 +43,21 @@ export default function HomePage() {
 
     const menuItems = [
       { icon: "schoolIcon", color: "#ff2972", index: 0 },
-      { icon: "settings-outline", color: "#fee800", index: 1 },
-      { icon: "mail-outline", color: "#04fc43", index: 2 },
-      { icon: "key-outline", color: "#fe00f1", index: 3 },
-      { icon: "camera-outline", color: "#00b0fe", index: 4 },
-      { icon: "game-controller-outline", color: "#fea600", index: 5 },
+      { icon: "engineeringIcon", color: "#fee800", index: 1 },
+      { icon: "contactMailIcon", color: "#fe00f1", index: 2 },
+      { icon: "sportsGymnasticsIcon", color: "#00b0fe", index: 3 },
+      { icon: "terminalIcon", color: "#04fc43", index: 4 },
+      { icon: "bookmarksIcon", color: "#fea600", index: 5 },
       { icon: "person-outline", color: "#a529ff", index: 6 },
       { icon: "videocam-outline", color: "#01bdab", index: 7 },
     ]
 
     return (
-      <>
-      <h1>Home Page</h1>
+      <div className="homepage-container">
+       <video autoPlay loop muted className="background-video">
+        <source src="/wave.mp4" type="video/mp4" />
+        Your browser does not support the video tag.
+      </video>
       <ul className="menu" ref={menuRef}>
       <div className="menuToggle" onClick={toggleMenu}>
         <IonIcon name="add-outline" />
@@ -57,11 +65,19 @@ export default function HomePage() {
       {menuItems.map(({ icon, color, index }) => (
         <li key={index} style={{ '--i': index, '--clr': color } as React.CSSProperties}>
          <a href="#" onClick={() => onClick(index)}>
-              {icon === "schoolIcon" ? <SchoolIcon className="mui-icon" /> : <IonIcon icon={icon} />}
+              {icon === "schoolIcon" ? <SchoolIcon className="mui-icon" /> : <></>}
+              {icon === "engineeringIcon" ? <EngineeringIcon className="mui-icon" /> : <></>}
+              {icon === "terminalIcon" ? <TerminalIcon className="mui-icon" /> : <></>}
+              {icon === "contactMailIcon" ? <ContactMailIcon className="mui-icon" /> : <></>}
+              {icon === "sportsGymnasticsIcon" ? <SportsGymnasticsIcon className="mui-icon" /> : <></>}
+              {icon === "bookmarksIcon" ? <BookmarksIcon className="mui-icon" /> : <></>}
+              {/* {icon === "sportsGymnasticsIcon" ? <SportsGymnasticsIcon className="mui-icon" /> : <></>}
+              {icon === "sportsGymnasticsIcon" ? <SportsGymnasticsIcon className="mui-icon" /> : <></>} */}
+
             </a>
         </li>
       ))}
     </ul>
-    </>
+    </div>
   );
 }
